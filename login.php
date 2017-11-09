b8446479290bdeff6ce790c878fc9d626cb26efc
OmarOdaini <ona11@msstate.edu>
2017-11-09 11:07:35 -0600

    Revert "No new code just cleaning up"
    This reverts commit 0c68167039c69a2028e34da9920838041139010a.

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Log In</title>
+  <title>Bootstrap Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
@@ -14,57 +14,42 @@
 <header>
     <!-- navbar -->
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-	
       <!-- Brand/logo -->
-      <a class="navbar-brand">
-    <!-- add logo Image !-->
+      <a class="navbar-brand" href="#">
+    <!-- add logo Image   !-->
       </a>
-	  
         <!--Search form -->
         <form class="form-inline" >
           <input  id="pull" class="form-control input-lg" type="text" placeholder="Search">
           <button class="btn btn-info" type="submit" id="pull">Search</button>
         </form>
-		
-		
-        <button type="button" class="btn btn-light" id="home"><a href="main.html"><b color="white">Home</b></a></button>
+        <button type="button" class="btn btn-light" id="home" href="#main.html"><a href="main.html"><b color="white">Home</b></a></button>
     </nav>
-	
   </header>
-  
-  
-  
-	<mid> 
-		<div class="row">
-		<div class="col-xs-6">  <div id="Logform">     <!--  Allow adjacent divs login and the big note-->
-		<div class="modal-dialog">
-		<div class="modal-content" id="padd"> <!--  adds boarders to the window!-->
-		<div class="modal-header"><h3>Log In</h3></div>
-						<form method="GET" action="AccountIn">
-						
-							<div class="form-group">
-								<label for="username" class="control-label">Username</label>
-								<input type="text" class="form-control" id="username" name="username" value="" required=""  placeholder="example@gmail.com">
-								<label for="password" class="control-label">Password</label>
-								<input type="password" class="form-control" id="password" name="password" value="" required="" placeholder="your password">
-							</div>
-							
-							<button type="submit" class="btn btn-success">Login</button>
-							<a href="#Optional" class="btn btn-default btn-block">Forgot My Password</a>
-							
-						</form>
-					</div> 
-				</div>
-			</div>
-		</div>
+  <mid>
+    <div class="row">
+    <div class="col-xs-6">  <div id="Logform">     <!--  Allow adjacent divs login and the big note-->
+         <div class="modal-dialog">
+            <div class="modal-content" id="padd"> <!--  adds boarders to the window!-->
+            <div class="modal-header"><h3>Log In</h3></div>
+                            <form method="GET" action="AccountIn">
+                                <div class="form-group">
+                                    <label for="username" class="control-label">Username</label>
+                                    <input type="text" class="form-control" id="username" name="username" value="" required="" title="Please enter you username" placeholder="example@gmail.com">
+                                    <label for="password" class="control-label">Password</label>
+                                    <input type="password" class="form-control" id="password" name="password" value="" required="" title="Please enter your password" placeholder="your password">
+                                </div>
+                                <button type="submit" class="btn btn-success">Login</button>
+                                <a href="#Optional" class="btn btn-default btn-block">Forgot My Password</a>
+                            </form>
+        </div>  </div>  </div></div>
 
-		<div class="col-xs-6"><h2 class="display-4 py-5 mt-5 text-right" id="note">Or join the family</h2>
-		<button type="button" class="btn btn-success" id="buttonLeft">Create an Account</button></div>  <!-- button !-->
+    <div class="col-xs-6"><h2 class="display-4 py-5 mt-5 text-right" id="note">Or join the family</h2>
+      <button type="button" class="btn btn-success" id="buttonLeft">Create an Account</button></div>  <!-- buttom !-- >
+</div>
 
 
   </mid>
-  
-  
 
 
 
--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Register</title>
+  <title>Bootstrap Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
@@ -11,49 +11,44 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
 </head>
 <body>
-
 <header>
     <!-- navbar -->
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-	
       <!-- Brand/logo -->
       <a class="navbar-brand" href="#">
     <!-- add logo Image   !-->
       </a>
-	  
         <!--Search form -->
         <form class="form-inline" >
           <input  id="pull" class="form-control input-lg" type="text" placeholder="Search">
           <button class="btn btn-info" type="submit" id="pull">Search</button>
         </form>
-		
-        <button type="button" class="btn btn-light" id="login"><a href="LogIn.html"><b>Log In</b></a></button>
-        <button type="button" class="btn btn-light" id="home"><a href="main.html"><b>Home</b></a></button>
+        <button type="button" class="btn btn-light" id="login" href="#main.html"><a href="LogIn.html"><b color="white">Log In</b></a></button>
+        <button type="button" class="btn btn-light" id="home" href="#main.html"><a href="main.html"><b color="white">Home</b></a></button>
     </nav>
   </header>
-  
-  
   <mid>
-		<div class="col-xs-6" id="Logform">    <!--  Allow adjacent divs login and the big note-->
-		<div class="modal-dialog">
-		<div class="modal-content" id="padd"> <!--  adds boarders to the window!-->
-		<div class="modal-header"><h3>Registeration</h3></div>
-						<form method="GET" action="AccountIn">
-							<div class="form-group">
-								<label for="username" class="control-label">First Name</label>
-								<input type="text" class="form-control" id="lastname" name="firstname" value="" required="" placeholder="First name">
-								<label for="username" class="control-label">Last name</label>
-								<input type="text" class="form-control" id="lastname" name="lastname" value="" required=""  placeholder="Last name">
-								<label for="username" class="control-label">Username</label>
-								<input type="text" class="form-control" id="username" name="username" value="" required=""  placeholder="example@gmail.com">
-								<label for="password" class="control-label">Password</label>
-								<input type="password" class="form-control" id="password" name="password" value="" required=""  placeholder="your password">
-								<label for="password" class="control-label">Confirm Password</label>
-								<input type="password" class="form-control" id="password" name="password" value="" required=""  placeholder="Confirm password">
-							</div>
-							<button type="submit" class="btn btn-success">Sign up</button>
-						</form>
-		</div>  </div> </div>
+    <div class="col-xs-6" id="Logform">    <!--  Allow adjacent divs login and the big note-->
+         <div class="modal-dialog">
+            <div class="modal-content" id="padd"> <!--  adds boarders to the window!-->
+            <div class="modal-header"><h3>Registeration</h3></div>
+                            <form method="GET" action="AccountIn">
+                                <div class="form-group">
+                                    <label for="username" class="control-label">First Name</label>
+                                    <input type="text" class="form-control" id="lastname" name="firstname" value="" required="" title="Please enter you username" placeholder="First name">
+                                    <label for="username" class="control-label">Last name</label>
+                                    <input type="text" class="form-control" id="lastname" name="lastname" value="" required="" title="Please enter you username" placeholder="Last name">
+                                    <label for="username" class="control-label">Username</label>
+                                    <input type="text" class="form-control" id="username" name="username" value="" required="" title="Please enter you username" placeholder="example@gmail.com">
+                                    <label for="password" class="control-label">Password</label>
+                                    <input type="password" class="form-control" id="password" name="password" value="" required="" title="Please enter your password" placeholder="your password">
+                                    <label for="password" class="control-label">Confirm Password</label>
+                                    <input type="password" class="form-control" id="password" name="password" value="" required="" title="Please enter your password" placeholder="Confirm password">
+                                </div>
+                                <button type="submit" class="btn btn-success">Sign up</button>
+                                <a href="#Optional" class="btn btn-default btn-block">Forgot My Password</a>
+                            </form>
+        </div>  </div> </div>
 
 
 
