f915757243808b8f94b6a9155a37b18de14ed96a
Steven Hillerman <31939792+shillerman@users.noreply.github.com>
2017-11-27 22:55:12 -0600

    Fixed broken link in Inventory

--- inventory.php
+++ inventory.php
@@ -13,9 +13,14 @@
   <script type="text/javascript">
    function Home(){  window.location='main.html';  }
   </script>
+<script type="text/javascript">
+	function logIn(){  window.location='login.php';  }
+	function shop(){  window.location='inventory.php';  }
+	function logOut() { window.location='logout.php'; }
+</script>
 </head>
 <body>
-    
+
     <!-- navbar -->
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <!-- Links -->
