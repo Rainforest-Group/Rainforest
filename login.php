cefef8d60f96c103525c18186e7d55a2a8d559d2
Steven Hillerman <31939792+shillerman@users.noreply.github.com>
2017-11-27 21:35:21 -0600

    fixed session login error

--- login.php
+++ login.php
@@ -6,30 +6,50 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="static/css/login.css">
+  <link rel="stylesheet" href="static/css/design.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
-    <script type="text/javascript">
+  <script type="text/javascript">
 	  function register(){  window.location='register.php';  }
 	   function Home(){  window.location='main.html';  }
+  </script>
+  <script type="text/javascript">
+	  function logIn(){  window.location='login.php';  }
+	   function shop(){  window.location='inventory.php';  }
+          function logOut() { window.location='logout.php'; }
   </script>
 </head>
 <body>
 <header>
     <!-- navbar -->
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-	
-      <!-- Brand/logo -->
-      <a class="navbar-brand">
-    <!-- add logo Image !-->
-      </a>
-	  
-        <!--Search form -->
-        <form class="form-inline" method="get" action="inventory.php">
-            <input class="form-control input-lg" type="text" placeholder="Search" name="q">
-            <button class="btn btn-info" type="submit" id="pull">Search</button>
-        </form>
-        <button type="button" class="btn btn-success" id="home" onclick="Home();">Home</button>
+        <!-- Links -->
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="navbar-brand" href="index.php">
+                    <img src="rainforest_logo.png" width="30" height="30">
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="admin.php">Administrators</a>
+            </li>
+                
+            <li>
+                <button type="button" class="btn btn-success" id="home" onclick="shop();">Shop</button>
+            </li>
+            <li>
+                <!--Search form -->
+                <form class="form-inline" method="get" action="inventory.php">
+                  <input class="form-control input-lg" type="text" placeholder="Search" name="q">
+                  <button class="btn btn-info" type="submit" id="pull">Search</button>
+                </form>
+            </li>
+            <?php 
+                require_once("controllers/indexController.php"); 
+                getLoginButton();
+            ?>
+        </ul>
     </nav>
 	
   </header>
