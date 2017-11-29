5c9402d98a725a2f9d2f7d22cbfd517c9b9b797a
Alex Coté <alx929@gmail.com>
2017-11-28 20:07:01 -0600

    Changed login credentials for database.

--- database/login_creds.php
+++ database/login_creds.php
@@ -2,5 +2,5 @@
     $hn = 'localhost';  // hostname
     $db = 'alc552';  // database name 
     $un = 'alc552';  // username (root on XAMPP, netID on pluto)
-    $pw = 'ab1234';  // MySQL password (empty on XAMPP, your MySQL password on pluto)
+    $pw = 'dcspg';  // MySQL password (empty on XAMPP, your MySQL password on pluto)
 ?>
