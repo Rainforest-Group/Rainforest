d0a226db763e7c7a315da66c94f02dff8e435bb9
Alex Coté <alx929@gmail.com>
2017-11-28 19:04:31 -0600

    Minor reformatting for maintainability.

--- logout.php
+++ logout.php
@@ -1,13 +1,11 @@
-    <?php
-        // remove session and session cookie
-        session_start();
-        $_SESSION = array();
-        setcookie(session_name(), '', time() - 1000, '/');
-        session_destroy();
+<?php
+    // remove session and session cookie
+    session_start();
+    $_SESSION = array();
+    setcookie(session_name(), '', time() - 1000, '/');
+    session_destroy();
 
-        // Redirect back to index
-        header("Location: index.php");
-        die();
-        
-    ?> 
-
+    // Redirect back to index
+    header("Location: index.php");
+    die();
+?> 
